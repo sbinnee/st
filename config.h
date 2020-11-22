cbbc77c63301f0dbb4188c8f15cdf12ad0f5837a
seongbin lim <sungbin246@gmail.com>
2020-11-22 22:05:14 +0100

    Let JetBrainsMono font with Non Ligature

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrains Mono:pixelsize=15:antialias=true:autohint=true";
+static char *font = "JetBrains Mono NL:pixelsize=15:antialias=true:autohint=true";
 /* static char *font = "DejaVu Sans Mono:pixelsize=15:antialias=true:autohint=true"; */
 /* Spare fonts */
 static char *font2[] = {
