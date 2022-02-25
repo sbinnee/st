e479613f1c27f157c07e7c8e576f9f7525b9def8
Seongbin Lim <sungbin246@gmail.com>
2022-02-25 22:33:10 +0100

    Patch font2

--- config.def.h
+++ config.def.h
@@ -5,7 +5,20 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "JetBrains Mono NL:pixelsize=15:antialias=true:autohint=true";
+/* static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true"; */
+/* Spare fonts */
+static char *font2[] = {
+	"Jetbrains Mono NL:pixelsize=15:antialias=true:autohint=true",
+	"JoyPixels:pixelsize=15:antialias=true:autohint=true",
+	"Hack Nerd Font Mono:pixelsize=15:antialias=true:autohint=true",
+	/* "NanumGothic:pixelsize=15:antialias=true:autohint=true", */
+	/* "Noto Sans Mono CJK JP:pixelsize=15:antialias=true:autohint=true", */
+	/* "Noto Sans Mono CJK SC:pixelsize=15:antialias=true:autohint=true", */
+	/* "Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
+	/* "Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
+};
+
 static int borderpx = 2;
 
 /*
