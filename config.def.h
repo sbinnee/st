a6c1f73abbdf1568e5524d1e7423f59c75e11d46
Seongbin Lim <sungbin246@gmail.com>
2022-02-25 22:44:31 +0100

    Patch dracula theme

--- config.def.h
+++ config.def.h
@@ -112,32 +112,32 @@ float alpha = 1.0;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
+	"#000000", /* black   */
+	"#ff5555", /* red     */
+	"#50fa7b", /* green   */
+	"#f1fa8c", /* yellow  */
+	"#bd93f9", /* blue    */
+	"#ff79c6", /* magenta */
+	"#8be9fd", /* cyan    */
+	"#bbbbbb", /* white   */
 
 	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+	"#44475a", /* black   */
+	"#ff5555", /* red     */
+	"#50fa7b", /* green   */
+	"#f1fa8c", /* yellow  */
+	"#bd93f9", /* blue    */
+	"#ff79c6", /* magenta */
+	"#8be9fd", /* cyan    */
+	"#ffffff", /* white   */
 
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
-	"gray90", /* default foreground colour */
-	"black", /* default background colour */
+	"#cccccc", /* 256 */
+	"#555555", /* 257 */
+	"#f8f8f2", /* 258 default foreground colour */
+	"#282a36", /* 259 default background colour */
 };
 
 
